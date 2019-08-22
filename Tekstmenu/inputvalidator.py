323bbfe939ea729962e412913c74af8df5c2650b
Martin RS <maros105@gmail.com>
2019-08-22 14:11:33 +0200

    Tilføjet kommentarer til koden

--- Tekstmenu/inputvalidator.py
+++ Tekstmenu/inputvalidator.py
@@ -1,10 +1,19 @@
 def getValidatedInput(besked, muligheder):
-    """Denne funktion modtager en introduktion, en besked og en liste af muligheder."""
+    """Denne funktion modtager en introduktion, en besked og en liste af muligheder.
+
+    Parametre:
+    besked - Besked til brugeren
+    muligheder - Liste af mulige lovlige indtastninger"""
+
     intro = 'Mulighederne er:\n'
+    # Byg en string af de mulige lovlige indtastninger
     for m in muligheder:
         intro = intro + m + ' '
     intro = intro + '\n'
+    # Modtag input fra brugren
     ind = input(intro + besked + ' ')
+    # Kontroller om indtastning er lovlig,
+    # ellers kald denne funktion igen med samme parametre
     if ind not in muligheder:
         ind = getValidatedInput(besked, muligheder)
     return ind
