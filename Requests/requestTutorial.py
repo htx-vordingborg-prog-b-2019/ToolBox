ba6f59c2d596eb6cdfbf10956e6737ca64eb5f6d
Nix <35922475+Lukas-Nix@users.noreply.github.com>
2019-09-26 12:29:32 +0200

    FUCK

--- Requests/requestTutorial.py
+++ Requests/requestTutorial.py
@@ -1,10 +1,13 @@
 import requests
 #Her importerer vi requests, som er et biblotek som kan håndtere at hente data fra en givent side
-brugerLink = input("Hvilket link vil du have data fra?")
+
+brugerLink = input("Hvilket link vil du have data fra?\n")
+
+
 LixText = requests.get(brugerLink)
 #Her importerer vi en side og definerer det til at være "LixText", men det er bare en variabel navn
 
-if LixText.status_code != 200:
+if LixText.status_code == 200:
     t = LixText.text
 else:
-    print("The status code of", "TEST" , "is", LixText.status_code)
+    print("The status code of", brugerLink , "is", LixText.status_code)
