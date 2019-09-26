db68d9ee74e7518462abd2520f42788d3cf320bd
Chongsenpai <37104800+Chongsenpai@users.noreply.github.com>
2019-09-26 12:22:29 +0200

    Update requestTutorial.py

--- Requests/requestTutorial.py
+++ Requests/requestTutorial.py
@@ -1,7 +1,7 @@
 import requests
 #Her importerer vi requests, som er et biblotek som kan håndtere at hente data fra en givent side
-
-LixText = requests.get('https://drive.google.com/drive/u/0/my-drive')
+brugerLink = input("Hvilket link vil du have data fra?")
+LixText = requests.get(brugerLink)
 #Her importerer vi en side og definerer det til at være "LixText", men det er bare en variabel navn
 
 t = LixText.text
