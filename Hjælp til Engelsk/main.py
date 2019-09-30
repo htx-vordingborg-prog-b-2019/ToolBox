287c77387fbeb094caa2f860670abbbc169bd271
Oliv6176 <32340667+Oliv6176@users.noreply.github.com>
2019-09-30 10:43:18 +0200

    HEYYEYEY

--- Hjælp til Engelsk/main.py
+++ Hjælp til Engelsk/main.py
@@ -1,6 +1,12 @@
-from PyDictionary import PyDictionary
-dictionary = PyDictionary()
-word = dictionary.meaning("House")
-for key in word.items() :
-    print (key)
+import requests
 
+url = "https://wordsapiv1.p.rapidapi.com/words/incredible/definitions"
+
+headers = {
+    'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
+    'x-rapidapi-key': "SIGN-UP-FOR-KEY"
+    }
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
