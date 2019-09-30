ec0f5b5c28e68f75d0f4f5ef3ef1e01ec7a0d26f
Skinkeve <Martinhansen4780@gmail.com>
2019-09-30 11:10:20 +0200

    Bilspil 1 notwork

--- Racegame/Racegame.py
+++ Racegame/Racegame.py
@@ -7,60 +7,59 @@ pygame.init()
 
 
 
-class spil:
+def variabler():
+    #her indsætter farverne i rbg
+    grey=(110, 110, 110)
+    white=(255,255,255)
+    black=(0,0,0)
+    red=(200,0,0)
+    green=(0,200,0)
+    blue=(0,0,200)
+    bright_red=(255,0,0)
+    bright_green=(0,255,0)
+    bright_blue=(0,0,255)
+    size = window_width, window_hight = 800, 600
+    #så displayer vi surfacet
+    gd=pygame.display.set_mode(size)
+    car_image=pygame.image.load('Bil.png')
+    car1=pygame.transform.scale(car_image, (100,100))
+    clock=pygame.time.Clock()
+    gameloop()
 
-    def __init__(self):
-        #her indsætter farverne i rbg
-        grey=(110, 110, 110)
-        white=(255,255,255)
-        black=(0,0,0)
-        red=(200,0,0)
-        green=(0,200,0)
-        blue=(0,0,200)
-        bright_red=(255,0,0)
-        bright_green=(0,255,0)
-        bright_blue=(0,0,255)
-        size = window_width, window_hight = 800, 600
-        #så displayer vi surfacet
-        gd=pygame.display.set_mode(size)
-        car_image=pygame.image.load('Bil.png')
-        car1=pygame.transform.scale(car_image, (100,100))
-        clock=pygame.time.Clock()
+def car(self,x,y):
+    gd.blit(car1,(x,y))
+    pygame.display.update()
 
-    def car(x,y):
-        gd.blit(car1,(x,y))
+
+def gameloop():
+    x_change = 0
+    lead_y = 0
+    x = 300
+    y = 490
+    block =  10
+    game_over = False
+    while game_over == False:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -block
+                elif event.key == pygame.K_RIGHT:
+                    x_change = +block
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    x_change = 0
+
+        x += x_change
+        #dette skaber en fejl nedenunder
+        gd.fill(black)
+        car(x,y)
+
+        clock.tick(60)
+
         pygame.display.update()
 
-
-    def gameloop():
-        x_change = 0
-        lead_y = 0
-        x = 300
-        y = 490
-        block =  10
-        game_over = False
-        while game_over == False:
-            for event in pygame.event.get ():
-                if event.type == pygame.QUIT:
-                    game_over = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT:
-                        x_change = -block
-                    elif event.key == pygame.K_RIGHT:
-                        x_change = +block
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                        x_change = 0
-
-            x += x_change
-            gd.fill(black)
-            car(x,y)
-
-            clock.tick(60)
-
-            pygame.display.update()
-
-gameloop()
-
+variabler()
 pygame.quit()
 quit()
